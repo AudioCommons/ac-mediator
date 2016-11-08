641ffc1c280b98791d44b5805e448f6cf4fdc14c
ffont <frederic.font@gmail.com>
2016-11-08 16:40:57 +0100

    Api resources are now function-based views instead of class-based

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import include, url
 from api import views
 
 urlpatterns = [
-    url(r'^services/$', views.Services.as_view()),
-    url(r'^search/text/$', views.TextSearch.as_view()),
-    url(r'^license/$', views.Licensing.as_view()),
+    url(r'^services/$', views.services),
+    url(r'^search/text/$', views.text_search),
+    url(r'^license/$', views.licensing),
 
     # Oauth2 urls
     url(r'^o/', include('api.oauth2_urls', namespace='oauth2_provider')),
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
-from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from api.request_distributor import get_request_distributor
 from api.response_aggregator import get_response_aggregator
 from services.management import get_available_services
@@ -9,49 +9,46 @@ request_distributor = get_request_distributor()
 response_aggregator = get_response_aggregator()
 
 
-class Services(GenericAPIView):
-
-    def get(self, request,  *args, **kwargs):
-        services = get_available_services()
-        return Response({
-            'count': len(services),
-            'services': {service.name: {
-                'id': service.id,
-                'url': service.url,
-                'components': service.implemented_components
-            } for service in services}
-        })
-
-
-class TextSearch(GenericAPIView):
-
-    def get(self, request,  *args, **kwargs):
-        response_id = request_distributor.process_request({
-            'component': SEARCH_TEXT_COMPONENT,
-            'method': 'text_search',
-            'kwargs': {'query': request.GET.get('q')}
-        })
-
-        # Because current implementation of request_distributor is synchronous, we can simply
-        # collect response here and return it. Otherwise we should probably simply return the
-        # response_id and the client should be in charge of iteratively checking if the response
-        # is ready to be returned
-        response = response_aggregator.collect_response(response_id)
-        return Response(response)
+@api_view(['GET'])
+def services(request):
+    services = get_available_services()
+    return Response({
+        'count': len(services),
+        'services': {service.name: {
+            'id': service.id,
+            'url': service.url,
+            'components': service.implemented_components
+        } for service in services}
+    })
 
 
-class Licensing(GenericAPIView):
+@api_view(['GET'])
+def text_search(request):
+    response_id = request_distributor.process_request({
+        'component': SEARCH_TEXT_COMPONENT,
+        'method': 'text_search',
+        'kwargs': {'query': request.GET.get('q')}
+    })
 
-    def get(self, request,  *args, **kwargs):
-        response_id = request_distributor.process_request({
-            'component': LICENSING_COMPONENT,
-            'method': 'get_licensing_url',
-            'kwargs': {'acid': request.GET.get('acid')}
-        })
+    # Because current implementation of request_distributor is synchronous, we can simply
+    # collect response here and return it. Otherwise we should probably simply return the
+    # response_id and the client should be in charge of iteratively checking if the response
+    # is ready to be returned
+    response = response_aggregator.collect_response(response_id)
+    return Response(response)
 
-        # Because current implementation of request_distributor is synchronous, we can simply
-        # collect response here and return it. Otherwise we should probably simply return the
-        # response_id and the client should be in charge of iteratively checking if the response
-        # is ready to be returned
-        response = response_aggregator.collect_response(response_id)
-        return Response(response)
+
+@api_view(['GET'])
+def licensing(request):
+    response_id = request_distributor.process_request({
+        'component': LICENSING_COMPONENT,
+        'method': 'get_licensing_url',
+        'kwargs': {'acid': request.GET.get('acid')}
+    })
+
+    # Because current implementation of request_distributor is synchronous, we can simply
+    # collect response here and return it. Otherwise we should probably simply return the
+    # response_id and the client should be in charge of iteratively checking if the response
+    # is ready to be returned
+    response = response_aggregator.collect_response(response_id)
+    return Response(response)
