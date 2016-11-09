b5f713a49a5321c7495f6baabaa6045a9c02aaef
ffont <frederic.font@gmail.com>
2016-11-09 12:13:26 +0100

    Update docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -144,7 +144,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "ac_sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
