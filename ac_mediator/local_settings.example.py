2bd8b3ad7eeb9a08f726e2e28582612e83bce9ab
ffont <frederic.font@gmail.com>
2016-11-10 12:16:33 +0100

    Update local settings for compatibility with docker

--- ac_mediator/local_settings.example.py
+++ ac_mediator/local_settings.example.py
@@ -1,3 +1,5 @@
+import os
+
 DEBUG = True
 ALLOWED_HOSTS = []
 SECRET_KEY = '090d2#wtg&q2@o+l%cvc&4)r4x5fr9o#r^qz3%0bemyecshn31'
@@ -6,8 +8,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ac_mediator',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': os.getenv('DOCKER_DB_HOST', '127.0.0.1')
     }
 }
 
