0e864a5f7417fc283455b5726e21162e84e05f53
ffont <frederic.font@gmail.com>
2016-11-10 16:03:01 +0100

    Attempt to read STATIC_ROOT from env variable

--- ac_mediator/settings.py
+++ ac_mediator/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.getenv('DOCKER_STATIC_ROOT', None)
 
 # Django rest framework (API)
 REST_FRAMEWORK = {
