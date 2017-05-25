fa426c68a3d7d5e03241a77cd827cfdd16cb199f
ffont <frederic.font@gmail.com>
2017-05-25 13:04:26 +0200

    Implement password reset using Django's provided views
    https://github.com/AudioCommons/ac-mediator/issues/1

--- ac_mediator/urls.py
+++ ac_mediator/urls.py
@@ -12,9 +12,23 @@ urlpatterns = [
 
     # Auth urls
     url(r'^register/$', registration, name='registration'),
+    url(r'^activate/(?P<username>[^\/]+)/(?P<uid_hash>[^\/]+)/.*$', activate_account, name="accounts-activate"),
     url(r'^login/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': settings.LOGOUT_URL}, name='logout'),
-    url(r'^activate/(?P<username>[^\/]+)/(?P<uid_hash>[^\/]+)/.*$', activate_account, name="accounts-activate"),
+    url(r'^password_reset/$', auth_views.password_reset, dict(
+            template_name='accounts/password_reset_form.html',
+            email_template_name='emails/password_reset.txt',
+        ), name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, dict(
+            template_name='accounts/password_reset_done.html'
+        ), name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, dict(
+            template_name='accounts/password_reset_confirm.html'
+        ), name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, dict(
+            template_name='accounts/password_reset_complete.html'
+        ), name='password_reset_complete'),
 
     # Accounts
     url(r'^', include('accounts.urls')),
