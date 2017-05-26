26d1742c40d1204995fffe3ec86c1b4452650019
ffont <frederic.font@gmail.com>
2017-05-26 11:56:56 +0200

    Add celery beat to compose file and add periodic tasks
    Periodic tasks are now configured in ac_mediator/celery.py
    
    https://github.com/AudioCommons/ac-mediator/issues/4

--- ac_mediator/celery.py
+++ ac_mediator/celery.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.core import management
+from celery.schedules import crontab
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ac_mediator.settings')
@@ -21,3 +24,18 @@ app.autodiscover_tasks('api', related_name='requests_distributor')
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+@app.task(bind=True)
+def run_django_management_command(self, command, *args, **kwargs):
+    management.call_command(command, *args, **kwargs)
+
+
+# Configure periodic tasks here
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(hour=6),  # Every day at 6 am
+        run_django_management_command.s('renew_access_tokens'),
+        name='Renew expired tokens')
+
