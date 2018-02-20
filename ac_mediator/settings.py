589e8be27dc13dfbfdced8854d9e8959a51e8d7d
miguel76 <miguel.ceriani@gmail.com>
2018-02-20 16:48:36 +0100

    CORS-enable the AC service

--- ac_mediator/settings.py
+++ ac_mediator/settings.py
@@ -55,17 +55,21 @@ INSTALLED_APPS = [
     'services',
     'docs',
     'raven.contrib.django.raven_compat',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'ac_mediator.urls'
 
