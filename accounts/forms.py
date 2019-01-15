5e243adba00ea4f17f01aee571f504803a55f874
Frederic Font <frederic.font@upf.edu>
2019-01-15 14:13:40 +0100

    Update terms of use section

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,8 +12,6 @@ class RegistrationForm(forms.Form):
         help_text=_("Required. 30 characters or fewer. Alphanumeric characters only (letters, digits and underscores)."),
         error_messages={'only_letters': _("This value must contain only letters, numbers and underscores.")}
     )
-    first_name = forms.CharField(help_text=_("Optional."), max_length=30, required=False)
-    last_name = forms.CharField(help_text=_("Optional."), max_length=30, required=False)
     email1 = forms.EmailField(label=_("Email"), help_text=_("We will send you a confirmation/activation email, so make "
                                                             "sure this is correct!."))
     email2 = forms.EmailField(label=_("Email confirmation"))
@@ -21,7 +19,8 @@ class RegistrationForm(forms.Form):
     password2 = forms.CharField(label=_("Password confirmation"), widget=forms.PasswordInput)
     accepted_tos = forms.BooleanField(
         label='',
-        help_text=_('Check this box to accept the <a href="###" target="_blank">terms of use</a>'),
+        help_text=_('Check this box to accept the <a href="/docs/api_tos.html" target="_blank">terms of service and '
+                    'privacy policy</a>'),
         required=True,
         error_messages={'required': _('You must accept the terms of use in order to register.')}
     )
@@ -57,13 +56,9 @@ class RegistrationForm(forms.Form):
         username = self.cleaned_data["username"]
         email = self.cleaned_data["email2"]
         password = self.cleaned_data["password2"]
-        first_name = self.cleaned_data.get("first_name", "")
-        last_name = self.cleaned_data.get("last_name", "")
         accepted_tos = self.cleaned_data.get("accepted_tos", False)
 
         account = Account(username=username,
-                          first_name=first_name,
-                          last_name=last_name,
                           email=email,
                           accepted_tos=accepted_tos)
         account.set_password(password)
